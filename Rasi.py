271e7cfe5d1bea8c49ce438bfb1d792ebefceb63
debaditya.mohankudo <debaditya.mohankudo@digicert.com>
2020-08-09 20:09:55 +0530

    draft

--- Rasi.py
+++ Rasi.py
@@ -3,3 +3,7 @@ class Rasi:
     def __init__(self, rasi_number, rasi_degree=Degree(0,0,0)):
         self.number = rasi_number
         self.degree = rasi_degree
+        
+    @property
+    def abs_degree(self):
+        return Degree(self.number * 30 - 30, 0, 0) + self.degree
