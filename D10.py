83b9bc16c6d1d039a9086a8db8bf921de54e949f
debaditya.mohankudo <debaditya.mohankudo@digicert.com>
2021-05-22 22:16:43 +0530

    d10

---
+++ D10.py
@@ -0,0 +1,10 @@
+from DivisionalChart import DivisionalChart
+from Rasi import Rasi
+
+
+class D10(DivisionalChart):
+
+    def __init__(self, rasi: Rasi):
+        super().__init__(rasi)
+        self.divisional_chart_no = 10
+
